26efabcab1815c8d0175f42843764197712c117e
kimbap <kuriimbap@gmail.com>
2026-09-18 16:23:40 +0700

    "เพิ่มคำนวณเลข"

--- hospital.cc
+++ hospital.cc
@@ -5,5 +5,13 @@ int main(){
     string name;
     cout << "your name : ";
     cin >> name;
-    cout << name;
+
+    int a,b;
+    int sum = 0;
+    cout << "First number : ";
+    cin >> a;
+    cout << "Second Number : ";
+    cin >> b;
+    sum = a+b;
+    cout << "Result is " << sum << endl;
 }
